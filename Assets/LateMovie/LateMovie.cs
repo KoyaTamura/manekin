80c2da7fe8ef7b70ad33c54e02b5a96bd0005da2
KoyaTamura <tamuchuchu@gmail.com>
2017-11-23 04:16:48 +0900

    commit
    add late time scene

--- Assets/LateMovie/LateMovie.cs
+++ Assets/LateMovie/LateMovie.cs
@@ -1,16 +1,93 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class LateMovie : MonoBehaviour {
 
+	//webcam数値
+	private WebCamTexture webcamTexture;
+	public int camNum = 0;
+	public int width = 1920;
+	public int height = 1080;
+	public int fps = 30;
+
+	//代入する云々
+	private Texture2D setTexture;
+	private Texture2DArray textureArray;
+	private bool webcamIsPlaying = true;
+
+	//何秒遅らせるか
+	public int lateSec = 3;
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+		WebCamInit ();
+		Application.targetFrameRate = fps; //60FPSに設定
+
 	}
-	
+		
+
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		//最初の一回で代入先のtextureを初期化
+		if(webcamTexture.width > 16 && webcamTexture.height > 16 && webcamIsPlaying){
+			SetTextureInit ();
+			webcamIsPlaying = false;
+		}
+
+		//全て初期化されたらここに入る
+		if(webcamIsPlaying == false){
+
+			textureArray.SetPixels (webcamTexture.GetPixels() , Time.frameCount % (lateSec * fps));
+			StartCoroutine ("LateTimeMovie",Time.frameCount % (fps * lateSec));
+
+//			textureArray.SetPixels (webcamTexture.GetPixels() , Time.frameCount % (lateSec * fps));
+//			setTexture.SetPixels (textureArray.GetPixels (Time.frameCount % (lateSec * fps)));
+//			setTexture.Apply ();
+
+		}
+
 	}
+
+	void WebCamInit(){
+
+		//webcamをスタートさせる
+		WebCamDevice[] devices = WebCamTexture.devices;
+		if (devices.Length > camNum)
+		{
+			webcamTexture = new WebCamTexture(devices[camNum].name, width, height, fps);
+			webcamTexture.Play();
+		}
+		else
+		{
+			Debug.Log("no camera");
+		}
+
+		//デバイスの名前一覧表示
+		for (int i = 0; i < devices.Length; i++)
+		{
+			Debug.Log(devices[i].name);
+		}
+	}
+
+	void SetTextureInit(){
+
+		//テクスチャー配列、カラー、表示テクスチャーの初期化
+		textureArray = new Texture2DArray(webcamTexture.width, webcamTexture.height,lateSec * fps, TextureFormat.RGB565,false);
+		//textureArray = null;
+		setTexture = new Texture2D(webcamTexture.width , webcamTexture.height,TextureFormat.RGB565,false);
+		GetComponent<Renderer> ().material.mainTexture = setTexture;
+	}
+
+	IEnumerator LateTimeMovie(int i){
+		yield return  new WaitForSeconds (1);
+		setTexture.SetPixels (textureArray.GetPixels (i));
+		setTexture.Apply ();
+	}
+		
 }
---
+++ Assets/LateMovie/colorLateMovie.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class colorLateMovie : MonoBehaviour {
+
+	public int camNum = 0;
+	public int width = 1920;
+	public int height = 1080;
+	public int fps = 30;
+
+	private WebCamTexture webcamTexture;
+
+
+	private Color32[] c;
+	private Color32[] LateColorNum;
+
+	private Texture2D texture;
+
+	public int lateSec = 10;
+
+	// Use this for initialization
+	void Start () {
+
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		if (devices.Length > camNum)
+		{
+			webcamTexture = new WebCamTexture(devices[camNum].name, width, height, fps);
+			webcamTexture.Play();
+		}
+		else
+		{
+			Debug.Log("no camera");
+		}
+
+		LateColorNum = new Color32[(fps * (lateSec + 1))];
+		c = new Color32[webcamTexture.width * webcamTexture.height];
+
+		texture = new Texture2D(webcamTexture.width, webcamTexture.height);
+		GetComponent<Renderer> ().material.mainTexture = texture;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//配列に入れる行為
+		c = webcamTexture.GetPixels32();
+		//LateColorNum [Time.frameCount % (fps * (lateSec + 1))] = c[];
+
+		//		myTex.SetPixels(c,Time.frameCount % (fps * (lateSec + 1)));
+		//		myTex.Apply();
+		//		//配列からtextureを引き抜く行為
+		//		GetCol.SetPixels (myTex.GetPixels (Time.frameCount % (fps * (lateSec + 1))));
+		//		GetCol.Apply ();
+		//		GetComponent<Renderer> ().material.mainTexture = GetCol;
+		//		//StartCoroutine ("LateTimeMovie",Time.frameCount % (fps * (lateSec + 1)));
+
+
+	}
+		
+}
